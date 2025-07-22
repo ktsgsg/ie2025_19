6d61d44d67fc9d35008f62641a24a89ec898c911
Toa Yamamoto <241205182@ccmailg.meijo-u.ac.jp>
2025-07-22 19:53:53 +0900

    フォントと各ページへのアクセス

--- js/script.js
+++ js/script.js
@@ -15,3 +15,16 @@ function buy(itemId){
     console.log("Item " + itemId + " added to cart. Total: " + cart[itemId]);
     $('#buyingmenu').css('visibility', 'visible');
 }
+
+window.addEventListener('load', function () {
+var $button = document.querySelector('.toggle-menu-button');
+var $menu = document.querySelector('.header-site-menu');
+$button.addEventListener('click', function () {
+if ($menu.classList.contains('is-show')) {
+$menu.classList.remove('is-show');
+}
+else {
+$menu.classList.add('is-show');
+}
+});
+});
